fc74416313ecc0027f9c0593fb5a312d18a7149a
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-11 12:37:25 +0700

    Menambahkan pengaturan untuk operasi pop dalam switch case dalam fungsi main

--- STACK.cpp
+++ STACK.cpp
@@ -80,9 +80,15 @@ int main(){
         
         switch (choice) {
             case 1:
-            cout << "Enter the value to push: ";
-            cin >> value;
-            stack.push(value); // push the entered value onto the stack
+                cout << "Enter the value to push: ";
+                cin >> value;
+                stack.push(value); // push the entered value onto the stack
+                break;
+            case 2:
+                if (!stack.isEmpty()){
+                    stack.pop(); // pop the top element from the stack
+                    
+                }
         }
     
     }
