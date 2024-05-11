608df2b75a74eacfde61cfc31e2e204d386f83b8
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-11 11:49:51 +0700

    penambahan penugasan data ke node baru dalam implementasi operasi push.

--- STACK.cpp
+++ STACK.cpp
@@ -24,6 +24,7 @@ class stack {
 
     // push operatiom: insert an element onto the  top of the stack
     int push(int value) {
+        Node* newNode = new Node(); // 1. allocate memory for the new node
         
     }
 
