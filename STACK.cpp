05661f108edc7c2221b129e2427205cb0701f895
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-11 11:44:04 +0700

    Menambahkan konstruktor untuk kelas stack dengan inisialisasi pointer 'top' menjadi NULL

--- STACK.cpp
+++ STACK.cpp
@@ -17,5 +17,10 @@ class stack {
     private:
     Node* top; // pointer to the top node of the stack
 
+    public:
+    stack(){
+        top = NULL; //initialize the stack with a null top pointer
+    }
+
 
 }
