d7d216a97c9fa525c5efde8882324e731309d2e4
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-11 12:46:31 +0700

    Menambahkan pengaturan untuk keluar dari program dalam switch case dan menangani pilihan tidak valid dalam fungsi main

--- STACK.cpp
+++ STACK.cpp
@@ -102,9 +102,19 @@ int main(){
                 }
                 break;
 
+            case 4:
+                cout << "Exiting program." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Try again." << endl;
+                break;
+
         }
+        cout << endl;
     
     }
+
+    return 0;
 
 }
  
