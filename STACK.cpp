34104ec43821afb9422802b4e4c07695f93fda4f
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-11 11:56:50 +0700

    Memperbarui pointer 'top' menjadi node baru setelah operasi push

--- STACK.cpp
+++ STACK.cpp
@@ -26,7 +26,8 @@ class stack {
     int push(int value) {
         Node* newNode = new Node(); // 1. allocate memory for the new node
         newNode->data = value; // 2. assign value
-        newNode->next = top; // 3. set the next poinnter of the new node to the current top node
+        newNode->next = top; // 3. set the next pointer of the new node to the current top node
+        top = newNode; // 4. update the top poinnter to the  new node
     }
 
 
